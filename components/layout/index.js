dd55f93221d362762eae5c2bca2712183b9e7645
Andrew Greig <andrew.s.greig@gmail.com>
2020-07-19 09:45:14 +1000

    fix: simplified boilerplate code

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,13 +1,21 @@
-import PropTypes from "prop-types";
-import React from "react";
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-import Meta from "../meta";
-import Wrapper from "../wrapper";
+import Wrapper from '../wrapper';
 
 const Layout = ({ meta, children }) => {
+  // Gmaps Src
+  const googleMapsKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const googleMapsSrc = `https://maps.googleapis.com/maps/api/js?key=${googleMapsKey}&libraries=geometry,drawing,places`;
+
   return (
     <>
-      <Meta {...meta} />
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <script src={googleMapsSrc}></script>
+      </Head>
       <Wrapper>{children}</Wrapper>
     </>
   );
